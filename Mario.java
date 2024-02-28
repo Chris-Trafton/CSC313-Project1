906a6956a2da0e8461bf63bedbcd0c1df78e86f3
nsdevries03 <nathansamuel.devries@calbaptist.edu>
2024-02-28 09:52:53 -0800

    added background

--- Mario.java
+++ Mario.java
@@ -1,12 +1,18 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Mario {
-    private static final int WIN_WIDTH = 800;
-    private static final int WIN_HEIGHT = 800;
+    private static final int WIN_WIDTH = 286;
+    private static final int WIN_HEIGHT = 286;
 
     private static JFrame frame;
+
+    private static BufferedImage background;
+    private static int xPos;
 
     public static void main(String[] args) {
         setup();
@@ -14,29 +20,40 @@ public class Mario {
         frame.setSize(WIN_WIDTH, WIN_HEIGHT);
         frame.setVisible(true);
 
-//        Thread t1 = new Thread(new Mario().new drawBackground());
-//        t1.start();
+        Thread t1 = new Thread(new Mario().new draw());
+        t1.start();
     }
 
     private static void setup() {
         frame = new JFrame("Mario");
+        xPos = 0;
+
+        String image = "";
+        try {
+            image = "background";
+            background = ImageIO.read(new File("images/World_1.png"));
+        } catch (IOException e) {
+            System.out.println(image + " not found.");
+        }
     }
 
     private class draw implements Runnable {
         public void run() {
             while (true) {
-                //draw background
+                drawBackground();
                 //draw mario
                 //draw enemies
-                delay(100);
+                delay(1);
             }
         }
     }
 
-    private class drawBackground implements Runnable {
-        public void run() {
+    private void drawBackground() {
+        Graphics g = frame.getGraphics();
+        Graphics2D g2d = (Graphics2D) g;
 
-        }
+
+        g2d.drawImage(background, xPos, 30, null);
     }
 
     private void delay(int n) {
